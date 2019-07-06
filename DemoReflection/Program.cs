fbc42ebc54c34cc2a4fa56202428145dfd674cea
wkq <172137365@qq.com>
2019-07-06 15:32:47 +0800

    通过反射创建对象

--- DemoReflection/Program.cs
+++ DemoReflection/Program.cs
@@ -16,15 +16,15 @@ namespace DemoReflection
             object obj = assembly.CreateInstance("DemoReflection.A"); //类的完全限定名（即包括命名空间）
             // 类A的构造有传参
             // 传参类型是int，但传入int，不会报错
-            object[] parameters3 = new object[1];
-            parameters3[0] = 123;
-            object obj3 = assembly.CreateInstance("DemoReflection.A",
-                true, System.Reflection.BindingFlags.Default, null, parameters3, null, null);// 创建类的实例
-            // 传参类型是int，但传入string，会报错
             object[] parameters2 = new object[1];
-            parameters2[0] = "123";
-            object obj2 = assembly.CreateInstance("DemoReflection.A", 
+            parameters2[0] = 123;
+            object obj2 = assembly.CreateInstance("DemoReflection.A",
                 true, System.Reflection.BindingFlags.Default, null, parameters2, null, null);// 创建类的实例
+            // 传参类型是int，但传入string，会报错
+            object[] parameters3 = new object[1];
+            parameters3[0] = "123";
+            object obj3 = assembly.CreateInstance("DemoReflection.A", 
+                true, System.Reflection.BindingFlags.Default, null, parameters3, null, null);// 创建类的实例
         }
     }
 }
